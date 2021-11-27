c938ca27eeeac69bc34d1e897331981cb3c0598e
cookie0215 <jina_0215@naver.com>
2021-11-27 23:46:38 +0900

    feat: READMD.md 파일 작성 및 구구단 표 수정
    Work:
    - READMD.md에 문제점과 해결방안 작성
    - 구구단 표 가로로 나열되던 내용을 세로로 작성

--- for/practice3_times tables/main.js
+++ for/practice3_times tables/main.js
@@ -13,21 +13,29 @@ let enableClick = true;
 
 btn.addEventListener('click',e=>{
     e.preventDefault();
-
+    
     if(enableClick){
         enableClick = false;
         timesTables();
     }
 })
-    
-function timesTables(){
-    for(let j=2; j<10; j++){
-        let tr = document.createElement("tr");
-        let th = document.createElement("th");
-        th.innerText = `${j}단`
-        tr.append(th);
 
-        for(let i=1; i<10; i++){
+function timesTables(){
+    
+    // 2단 ~9단 표 제목 생성하는 DOM
+    const tr = document.createElement("tr");
+    for(let j=2; j<10; j++){
+        let th = document.createElement("th");
+        th.innerText = `${j}단`;
+        div.append(table);
+            table.append(tr);
+        tr.append(th);
+    }
+
+    // 각 구구단에 1~9까지 계산된 값
+    for(let i=1; i<10; i++){
+        const tr = document.createElement("tr");
+        for(let j=2; j<10; j++){
             let td = document.createElement("td");
             td.innerText = `${j}*${i} = ${j*i}`;
             
